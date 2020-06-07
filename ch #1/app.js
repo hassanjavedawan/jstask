b351305ed8c73bae76fa7ca8fcd9fbdc3aa836ed
hassanjavedawan <hassanjaved836@gmail.com>
2020-06-07 16:54:47 +0500

    incomplete

---
+++ ch #1/app.js
@@ -0,0 +1,14 @@
+//1,2 exercise
+alert("Error! Please enter valid password");
+
+//3 exercise
+alert("Welcome to JS Land....\n Happy Codding!");
+
+//4 exercise
+alert("Welcome to JS Land....");
+alert("Happy Codding\n \n ⬜ Prevent this page from creating additional dialogs" );
+
+//5 exercise
+
+ console.log(alert("Hello...I can run JS through my web browser's console"));
+
---
+++ ch #2/app.js
@@ -0,0 +1,45 @@
+// 1 exercise
+var username;
+
+// 2 exercise
+var myname;
+myname = ("Hassan Javed");
+
+// 3 exersice
+var a;
+a = ("Hello Word");
+alert(a);
+
+// 4 exersice student bio data
+var name =( "Hassan Javed");
+var age = ("21 years old ");
+var b = ("Certified Mobile Application Development ");
+alert(name);
+alert(age);
+alert(b);
+
+
+// 5 exercise
+var x;
+x = ("PIZZA \n PIZZ \n PIZ \n PI \n P");
+alert(x);
+
+// 6 exercise 
+var email;
+email = (" My email address is hassanjaved836@gmail.com");
+alert(email);
+
+// 7 exercise
+var book;
+book = ("I am trying to learn from Book A smarter \n way to learn JavaScript");
+alert(book);
+
+// 8 exercise
+var y = ("Yah! I can write HTML content through Javascipt");
+document.write(y);
+
+// 9 exercise
+var design;
+design = ("-------");
+// alert(design);
+// document.write(alert("------"));
